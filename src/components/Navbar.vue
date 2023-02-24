070b43320c8db37e73909e39c03c4336714cc330
Asadbek <asadbeknan5566@gmail.com>
2023-02-24 23:16:31 +0500

    user interface

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,11 @@
 <template>
-  <nav class="flex justify-between container py-2 items-center border-b">
+  <nav class="flex px-2 justify-between container py-2 items-center border-b">
     <RouterLink tag="h1" :to="{ name: 'home' }" class="text-[35px]"
       >Asad</RouterLink
     >
     <template v-if="registeredUser">
       <RouterLink :to="{ name: 'home' }">Profile</RouterLink>
-      <h1 class="bg-green-900 px-2">
-        {{ currentUser?.username }}
-      </h1>
+
       <Button @click="logout">Log out</Button>
     </template>
     <template v-if="anonymousUser">
@@ -15,14 +13,19 @@
         <li>
           <RouterLink :to="{ name: 'login' }">Login</RouterLink>
         </li>
-        <RouterLink :to="{ name: 'register' }">Register</RouterLink>
+        <li>
+          <RouterLink :to="{ name: 'register' }">Register</RouterLink>
+        </li>
       </ul>
     </template>
   </nav>
+  <div>
+    <h1 class="bg-green-900 px-2">Welcome: {{ currentUser?.username }}</h1>
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapState } from "vuex";
+import { mapGetters } from "vuex";
 import { logo } from "../constants";
 import { getterType } from "../modules/types";
 
